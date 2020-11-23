a4bcbea6ff21f9ebc8bf346b509146ed320123e7
alessiogarifo92 <alessiogarifo92@gmail.com>
2020-11-23 15:03:37 +0100

    prove prossimo milestone agg testo chat

--- js/script.js
+++ js/script.js
@@ -6,12 +6,20 @@
 var app = new Vue({
   el : '#app',
   data : {
-    activeEl : 0
+    activeEl : 0,
+    chat : [],
+    addElement : '',
+    rispostaFissa: []
   },
 
   methods : {
     mostraChat : function (el) {
       this.activeEl = el;
+    },
+    addItem(){
+      this.chat.push(this.addElement);
+      this.addElement = '';
+      this.rispostaFissa.push('ok...')
     }
   }
 });
